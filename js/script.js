01295c3ffa0f8f7cdbf3781b13ede6694b280755
Faridd13 <faridalwaritsi4@gmail.com>
2024-11-29 20:47:10 +0700

    update 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("Portfolio website loaded successfully!");
+// console.log("Portfolio website loaded successfully!");
+
+const port = 8080; // Ubah dari port sebelumnya
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
